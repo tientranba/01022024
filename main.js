59c5dcdf5c1017f2d61dfb313b8eed676712551b
tientranba <108992278+tientranba@users.noreply.github.com>
2024-02-05 22:36:48 +0700

    Update main.js

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ function checkLogin() {
     var tenBe = document.getElementById('tenBe').value;
     var ngaySinhBe = document.getElementById('ngaySinhBe').value;
   
-    // Kiểm tra thông tin đăng nhập (thay thế điều kiện này bằng thông tin thực của bạn)
+    
     if (tenBe === 'ngocthao' && ngaySinhBe === '09012005') {
-      // Chuyển hướng đến trang Google.com khi đăng nhập đúng
+  
       window.location.href = 'https://tientranba.github.io/countlove/';
     } else {
       alert('Có phải bồ tui khong đó???');
